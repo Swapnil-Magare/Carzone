777a7fa4ba477fec889cd8dc42801e5d2b1458a5
RoshanSonavane2025 <roshancybernxt@gmail.com>
2025-03-27 16:56:10 +0530

    Updated app.js for updating operations

--- app.js
+++ app.js
@@ -2,34 +2,28 @@ let company=document.getElementById('company')
 let title=document.getElementById("title")
 // console.log(company)
 let cd=document.getElementById('company_data')
-console.log(cd);
+// console.log(cd);
 let title2=document.getElementById("title2")
 let c1=document.getElementById("car")
 let b1=document.getElementById("back")
+let b=0;
 
 
 
+let obj={"operation":"car"}
+sessionStorage.setItem('formData', JSON.stringify(obj))
 
 let data=window.fetch("./data.json")
-data.then(    (d)=>{
+data.then( (d)=>{
     let fD=d.json()
     fD.then(
         (final)=>{
             // console.log(final)
             for (let i of final){
                 let {name,location,id}=i
-                company.innerHTML+=`<section id="${id}" onclick=clk(event,id) class="company_name ${name}"><p>${name}</p><address>${location}</address>
-                <button class="operations"><a href="http://localhost:8080/company/add
-">ADD</a></button>
-                <button class="operations"><a href="http://localhost:8080/company/update/${id}
-">UPDATE</a></button>
-                <button class="operations"><a href="http://localhost:8080/company/delete/${id}
-">DELETE</a></button>
-                <button class="operations"><a href="http://localhost:8080/company/get/${id}
-">INFO</a></button>
-                
-                </section>`
+                company.innerHTML+=`<section id="${id}" onclick=clk(event,id) class="company_name ${name}"><p>${name}</p><address>${location}</address></section>`
             }
+
         },
         (err)=>console.log(err)
     )
@@ -38,9 +32,9 @@ data.then(    (d)=>{
 )
 
 async function clk(event,id){
+    b=1
     company.style.display="none"
     b1.style.display="inline"
-    console.log(title);
     cd.style.display="flex"
     let data=window.fetch("./data.json")
         data.then(    (d)=>{
@@ -59,19 +53,29 @@ async function clk(event,id){
                                 <tr class="car_data">
                                 <td>${No}</td>
                                 <td>${model}</td> 
-                                <td>${year}</td> 
-                                <td id="operationss">
-                                <button class="operations"><a href="http://localhost:8080/car/add
-">ADD</a></button>
-                                <button class="operations"><a href="http://localhost:8080/car/update/${id}
-">UPDATE</a></button>
-                                <button class="operations"><a href="http://localhost:8080/car/delete/${id}
-">DELETE</a></button>
-                                <button class="operations"><a href="http://localhost:8080/car/${id}
-">INFO</a></button>
-                                </td>    
+                                <td>${year}</td>   
                                 </tr>`
                             }
+                            let obj={"operation":"car11"}
+                            sessionStorage.setItem('formData', JSON.stringify(obj))
+                            let kt=document.getElementById(`add`)
+                            kt.addEventListener("click",(v)=>{
+                            let obj={"operation":"add_car"}
+                            sessionStorage.setItem('formData', JSON.stringify(obj));
+                            window.location.href = "operation.html";})
+
+                            let k1=document.getElementById(`update`)
+                            k1.addEventListener("click",(v)=>{
+                                let obj={"operation":"update_car"}
+                                sessionStorage.setItem('formData', JSON.stringify(obj));
+                                window.location.href = "operation.html";
+
+                            })
+                            let k2=document.getElementById(`delete`)
+                            k2.addEventListener("click",(v)=>{
+                                let obj={"operation":"delete_car"}
+                                sessionStorage.setItem('formData', JSON.stringify(obj));
+                                window.location.href = "operation.html";})
                     }}
                 },
                 (err)=>console.log(err)
@@ -80,4 +84,33 @@ async function clk(event,id){
         (e)=>console.log(e)
 )
     
+}
+
+
+const formData = JSON.parse(sessionStorage.getItem('formData'));
+console.log(formData.operation,"server")
+
+
+if (formData.operation=="car"){
+console.log("aaa");
+let k=document.getElementById(`add`)
+k.addEventListener("click",(v)=>{
+let obj={"operation":"add"}
+sessionStorage.setItem('formData', JSON.stringify(obj));
+window.location.href = "operation.html";})
+
+let k1=document.getElementById(`update`)
+k1.addEventListener("click",(v)=>{
+    let obj={"operation":"update"}
+    sessionStorage.setItem('formData', JSON.stringify(obj));
+    window.location.href = "operation.html";
+
+})
+let k2=document.getElementById(`delete`)
+k2.addEventListener("click",(v)=>{
+    let obj={"operation":"delete"}
+    sessionStorage.setItem('formData', JSON.stringify(obj));
+    window.location.href = "operation.html";
+
+})
 }
