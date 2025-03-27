70d0a8efc1b8040f0dbab6a47c012949d2b425f6
Vedantp15 <vedantpatil1503@gmail.com>
2025-03-27 10:22:39 +0530

    Add CompanyService.java from vedant branch

--- Carzone/src/main/java/com/carzone/service/CompanyService.java
+++ Carzone/src/main/java/com/carzone/service/CompanyService.java
@@ -1,55 +1,103 @@
 package com.carzone.service;
 
-//import com.carzone.controller.CompanyDto;
 import com.carzone.dto.CompanyDto;
+import com.carzone.dto.ResponseStructure;
+import com.carzone.model.Car;
 import com.carzone.model.Company;
+import com.carzone.repositoy.CarRepository;
 import com.carzone.repositoy.CompanyRepository;
+import com.carzone.serviceInterface.CompanyInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CompanyService {
+public class CompanyService implements CompanyInterface {
 
     @Autowired
     private CompanyRepository companyRepository;
 
-    public Company addCompany(Company company) {
-        return companyRepository.save(company);
+    @Autowired
+    private CarRepository carRepository;
+
+    @Override
+    public ResponseEntity<ResponseStructure<Company>> addCompany(CompanyDto companyDto) {
+        Company company = new Company();
+        company.setName(companyDto.getName());
+        company.setLocation(companyDto.getLocation());
+        if (companyDto.getCars() != null) {
+            List<Car> cars = new ArrayList<>();
+            for (Car carDto : companyDto.getCars()) {
+                Car car = new Car();
+                car.setModel(carDto.getModel());
+                car.setYear(carDto.getYear());
+                car.setCompany(company);
+                cars.add(car);
+            }
+            company.setCars(cars);
+        }
+        Company savedCompany = companyRepository.save(company);
+//        CompanyDto savedCompanyDto = new CompanyDto(savedCompany.getName(), savedCompany.getLocation(), carDtoList);
+        ResponseStructure<Company> responseStructure = new ResponseStructure<>(HttpStatus.OK.value(), "Company and cars saved successfully!", savedCompany);
+        return new ResponseEntity<>(responseStructure, HttpStatus.OK);    }
+
+
+        @Override
+    public ResponseEntity<ResponseStructure<List<Company>>> getAllCompany() {
+        List<Company> companies = companyRepository.findAll();
+        ResponseStructure<List<Company>> responseStructure = new ResponseStructure<List<Company>>(HttpStatus.OK.value(),"Show All Companies ",companies);
+        return new ResponseEntity<ResponseStructure<List<Company>>>(responseStructure, HttpStatus.OK);
     }
 
-    public List<Company> getAllCompany() {
-        return companyRepository.findAll();
-    }
-
-    public Optional<Company> getByCompanyId(Long id) {
-        return companyRepository.findById(id);
-    }
-
-    public void deleteByCompanyid(Long id) {
+    @Override
+    public ResponseEntity<ResponseStructure<Company>> getCompanyById(long id){
         Optional<Company> optional = companyRepository.findById(id);
+        if (optional.isPresent()){
+            Company company = optional.get();
+            ResponseStructure<Company> responseStructure = new ResponseStructure<Company>(HttpStatus.OK.value(),"Company Fetched Successfully.",company);
+            return new ResponseEntity<ResponseStructure<Company>>(responseStructure, HttpStatus.OK);
+        }
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ResponseStructure<Company>> updateCompanyName(long id, CompanyDto companyDtores) {
+        Optional<Company> optional = companyRepository.findById(id);
+
         if (optional.isPresent()) {
             Company company = optional.get();
-            companyRepository.delete(company);
+            company.setName(companyDtores.getName());
+            company.setLocation(companyDtores.getLocation());
+            Company save = companyRepository.save(company);
+            ResponseStructure<Company> responseStructure = new ResponseStructure<Company>(HttpStatus.OK.value(), "Company Updated Successfully.", save);
+            return new ResponseEntity<ResponseStructure<Company>>(responseStructure, HttpStatus.OK);
+        } else {
+            ResponseStructure<Company> responseStructure = new ResponseStructure<Company>(HttpStatus.NOT_FOUND.value(), "Company not found.", null);
+            return new ResponseEntity<ResponseStructure<Company>>(responseStructure, HttpStatus.NOT_FOUND);
         }
     }
 
-    public CompanyDto updateById(Long id, CompanyDto companyDto) {
-        Optional<Company> optionalCompany = companyRepository.findById(id);
-        if (optionalCompany.isPresent()) {
-            Company company = optionalCompany.get();
-            company.setName(companyDto.getName());
-            company.setLocation(companyDto.getLocation());
-            companyRepository.save(company);
+    @Override
+    public ResponseEntity<ResponseStructure<CompanyDto>> deleteCompany(long id) {
+        Optional<Company> optional = companyRepository.findById(id);
+        if (optional.isPresent()) {
+            Company company = optional.get();
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.getName();
+            companyDto.getLocation();
+            companyDto.getCars();
+            companyRepository.delete(company);
+
+            ResponseStructure<CompanyDto> responseStructure = new ResponseStructure<>(HttpStatus.OK.value(), "Company Deleted Successfully.", companyDto);
+            return new ResponseEntity<>(responseStructure, HttpStatus.OK);
+        }
+        ResponseStructure<CompanyDto> responseStructure = new ResponseStructure<>(HttpStatus.NOT_FOUND.value(), "Company not found.", null);
+        return new ResponseEntity<>(responseStructure, HttpStatus.NOT_FOUND);
     }
-        return null;
-}
-
-
-
-
-
 
 }
